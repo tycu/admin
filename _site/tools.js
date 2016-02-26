c37fdf83059cd65be41be1c5715beaf2645a294b
Ryan Oldenburg <guzba8@gmail.com>
2016-02-25 22:59:56 -0800

    use prod in prod, dev locally

--- _site/tools.js
+++ _site/tools.js
@@ -7,7 +7,7 @@ var space = function() {
 }
 
 var host = function() {
-    if (localStorage.server == 'production') {
+    if (location.host == 'production') {
         return 'https://api.tally.us'
     } else {
         return 'http://localhost:5000'
@@ -20,43 +20,9 @@ document.addEventListener('DOMContentLoaded', function(event) {
     title.id = 'title'
     title.textContent = 'Tally'
 
-    var dev = document.createElement('span')
-    dev.style.display = 'inline-block'
-    dev.style.marginLeft = '28px'
-
-    var prod = document.createElement('span')
-    prod.style.display = 'inline-block'
-    prod.style.marginLeft = '14px'
-
-    var devRadio = document.createElement('input')
-    devRadio.id = 'dev'
-    devRadio.type = 'radio'
-    devRadio.name = 'server'
-
-    var prodRadio = document.createElement('input')
-    prodRadio.id = 'prod'
-    prodRadio.type = 'radio'
-    prodRadio.name = 'server'
-
-    var devLabel = document.createElement('label')
-    devLabel.htmlFor = devRadio.id
-    devLabel.textContent = ' development'
-    
-    var prodLabel = document.createElement('label')
-    prodLabel.htmlFor = prodRadio.id
-    prodLabel.textContent = ' production'
-
-    dev.appendChild(devRadio)
-    dev.appendChild(devLabel)
-
-    prod.appendChild(prodRadio)
-    prod.appendChild(prodLabel)
-
     var header = document.createElement('div')
     header.id = 'header'
     header.appendChild(title)
-    header.appendChild(dev)
-    header.appendChild(prod)
 
     var adminKey = document.createElement('input')
     adminKey.type = 'text'
@@ -69,26 +35,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
     document.body.insertBefore(header, document.body.firstChild)
 
-    if (localStorage.server == 'production') {
-        prodRadio.checked = true
-    } else {
-        devRadio.checked = true
-    }
-
-    devRadio.onchange = function() {
-        if (devRadio.checked) {
-            localStorage.server = 'development'
-        }
-        location.reload()
-    }
-
-    prodRadio.onchange = function() {
-        if (prodRadio.checked) {
-            localStorage.server = 'production'
-        }
-        location.reload()
-    }
-
     adminKey.value = localStorage.adminKey || ''
 
     adminKey.onkeypress = function(e) {
@@ -96,5 +42,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
             localStorage.adminKey = adminKey.value
             location.reload()
         }
+    }
+
+    if (!localStorage.adminKey) {
+        window.onload = null
+        document.body.firstChild.nextElementSibling.innerHTML = 'Admin key required'
     }
 })
