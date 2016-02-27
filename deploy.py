5e003602cc4991a863eadea7b27a4a729eee6942
Ryan Oldenburg <guzba8@gmail.com>
2016-02-26 23:33:52 -0800

    hopefully stop caching

--- deploy.py
+++ deploy.py
@@ -1,3 +1,3 @@
 import os
 import subprocess
-subprocess.call("gsutil -m rsync -d -r _site gs://admin.tally.us/", cwd=os.getcwd(), shell=True)
+subprocess.call("gsutil -h \"Cache-Control: no-cache\" -m rsync -d -r _site gs://admin.tally.us/", cwd=os.getcwd(), shell=True)
