d3fa9338b1cc31675f52f387a0a21e412d9c9bf9
Ryan Oldenburg <guzba8@gmail.com>
2016-02-29 18:34:55 -0800

    redirect http

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ var port = process.env.PORT || 5001
 var start = function() {
     var express = require('express')
     var app = express()
+
+    // Require HTTPS in production
+    if (process.env.NODE_ENV == 'production') {
+        app.use(function(req, res, next) {
+            if (req.headers['x-forwarded-proto'] !== 'https') {
+                res.redirect(301, 'https://' + req.get('Host') + req.url)
+            } else {
+                next()
+            }
+        })
+    }
     
     app.use(express.static('public'))
 
